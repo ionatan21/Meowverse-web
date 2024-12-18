5e392d5662ef226207c8d7941c9d506ed363ea92
UCR <barriosjonatan1221@gmail.com>
2024-12-18 15:55:55 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Meowverse</h1>
+      <div className="card">
+        <p>Your comprehensive resource for raising happy and healthy cats!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
